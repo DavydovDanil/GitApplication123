03c5cac0aed14ee5911845429df24577e47b7de2
Horse <vintaikin.kirill@yandex.ru>
2023-01-31 22:34:53 +0300

    Добавлен метод МатриксВ

--- GitApplication123/Program.cs
+++ GitApplication123/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Gitapplication 
+{
+  class Program
+    {
+
+        static void Main() 
+        {
+        
+        }
+        public void MatrixB(int n, int m)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            int[,] mass = new int[n, m];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m - 1; ++j)
+                {
+                    if (i == j)
+                        mass[i, j + 1] = i + j;
+                }
+            }
+            for (int i = 0; i < n; ++i, Console.WriteLine())
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+            }
+        }
+    }
+}
